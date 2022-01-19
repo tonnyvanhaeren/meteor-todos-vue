63f5ef00148f679bf5a6db9bdcb638f4bab3005f
Antonius <antonius.vanhaeren.av@gmail.com>
2022-01-19 08:10:31 +0100

    adding User accounts and secret settings

--- server/main.js
+++ server/main.js
@@ -1,7 +1,12 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 import { TasksCollection } from '/imports/api/TasksCollection';
 
 const insertTask = (taskText) => TasksCollection.insert({ text: taskText });
+
+const admin_username = Meteor.settings.userAdmin.username;
+const admin_email = Meteor.settings.userAdmin.email;
+const admin_password = Meteor.settings.userAdmin.password;
 
 Meteor.startup(() => {
   if (TasksCollection.find().count() === 0) {
@@ -14,5 +19,14 @@ Meteor.startup(() => {
       'Sixth Task',
       'Seventh Task',
     ].forEach(insertTask);
+  }
+
+  // create default admin user
+  if (!Accounts.findUserByUsername(admin_username)) {
+    Accounts.createUser({
+      email: admin_email,
+      username: admin_username,
+      password: admin_password,
+    });
   }
 });
