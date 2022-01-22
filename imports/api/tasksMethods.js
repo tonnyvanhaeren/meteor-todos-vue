beb088abe04b25ade960aad5ce79a5b7e1af674a
Antonius <antonius.vanhaeren.av@gmail.com>
2022-01-22 08:16:23 +0100

    Start with mocha chai testing

--- imports/api/tasksMethods.js
+++ imports/api/tasksMethods.js
@@ -24,6 +24,12 @@ Meteor.methods({
       throw new Meteor.Error('Not authorized.');
     }
 
+    const task = TasksCollection.findOne({ _id: taskId, userId: this.userId });
+
+    if (!task) {
+      throw new Meteor.Error('Access denied.');
+    }
+
     TasksCollection.remove(taskId);
   },
 
--- tests/main.js
+++ tests/main.js
@@ -1,20 +1,25 @@
-import assert from "assert";
+/* eslint-env mocha */
 
-describe("skel", function () {
-  it("package.json has correct name", async function () {
-    const { name } = await import("../package.json");
-    assert.strictEqual(name, "skel");
+import { Meteor } from 'meteor/meteor';
+import assert from 'assert';
+
+describe('Package name', function () {
+  it('package.json has correct name', async function () {
+    const { name } = await import('../package.json');
+    assert.strictEqual(name, 'simple-todos-vue');
   });
 
   if (Meteor.isClient) {
-    it("client is not server", function () {
+    it('client is not server', function () {
       assert.strictEqual(Meteor.isServer, false);
     });
   }
 
   if (Meteor.isServer) {
-    it("server is not client", function () {
+    it('server is not client', function () {
       assert.strictEqual(Meteor.isClient, false);
     });
   }
 });
+
+import '/imports/api/tasksMethods.tests';
